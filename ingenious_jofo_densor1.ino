1febe8cdbf4afececd4b26796bb10ac508ab1c80
SalwaMukhtar <102853846+SalwaMukhtar@users.noreply.github.com>
2022-04-02 14:03:43 +0500

    Add files via upload
    circuit design

---
+++ ingenious_jofo_densor1.ino
@@ -0,0 +1,54 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(3, OUTPUT);
+  pinMode(5, OUTPUT); 
+  pinMode(7, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(3, HIGH);
+  delay(1000); 
+  digitalWrite(3, LOW);
+  delay(1000);
+  digitalWrite(5, HIGH);
+  delay(1000); 
+  digitalWrite(5, LOW);
+  delay(1000);
+  digitalWrite(7, HIGH);
+  delay(1000); 
+  digitalWrite(7, LOW);
+  delay(1000);
+  digitalWrite(10, HIGH);
+  delay(1000); 
+  digitalWrite(10, LOW);
+  delay(1000);
+  digitalWrite(11, HIGH);
+  delay(1000); 
+  digitalWrite(11, LOW);
+  delay(1000);
+  digitalWrite(10, HIGH);
+  delay(1000); 
+  digitalWrite(10, LOW);
+  delay(1000);
+  digitalWrite(7, HIGH);
+  delay(1000); 
+  digitalWrite(7, LOW);
+  delay(1000);
+  digitalWrite(5, HIGH);
+  delay(1000); 
+  digitalWrite(5, LOW);
+  delay(1000);
+  digitalWrite(3, HIGH);
+  delay(1000); 
+  digitalWrite(3, LOW);
+  delay(1000);
+  digitalWrite(11, HIGH);
+  delay(1000); 
+  digitalWrite(11, LOW);
+  delay(1000);
+}
